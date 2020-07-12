22ce466f3539964695f4dae44972866f55a5dffa
Mahesh J <mahesh.j567@gmail.com>
2020-07-13 00:39:46 +0530

    first commit

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import DateRangePickerExporter from './components/DateRangePickerExporter';
+import { DateRange, DefinedRange } from './types';
+export { DateRangePickerExporter as DateRangePicker, DefinedRange, DateRange, };
